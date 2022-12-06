ab87d17bbcbcc57b6e1816b7707e974cfd3255f1
Ion Gataman <ion.gatman1@isa.utm.md>
2022-12-06 13:53:25 +0200

    - Added Pageable to all endpoints - Added ResponseEntity to AdressController

--- src/main/java/com/example/fakerwithauthorization/controllers/AddressController.java
+++ src/main/java/com/example/fakerwithauthorization/controllers/AddressController.java
@@ -3,6 +3,9 @@ package com.example.fakerwithauthorization.controllers;
 
 import com.example.fakerwithauthorization.models.Address;
 import com.example.fakerwithauthorization.repository.AddressRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,27 +19,25 @@ public class AddressController {
     }
 
     @PostMapping
-    public String addAdress(@RequestBody Address address){
-        System.out.println(address.toString());
-        addressRepository.save(address);
-        return "Adress aded";
+    public ResponseEntity<Address> addAdress(@RequestBody Address address){
+
+        return ResponseEntity.ok().body(addressRepository.save(address));
     }
 
-    @GetMapping("/get")
-    public Iterable<Address> getAll(){
-        return addressRepository.findAll();
+    @GetMapping
+    public ResponseEntity<Page<Address>> getAll(Pageable pageable){
+        return  ResponseEntity.ok().body(addressRepository.findAll(pageable));
     }
 
     @PutMapping
-    public Address updateAdress(@RequestBody Address address){
-        addressRepository.save(address);
-        return address;
+    public ResponseEntity<Address> updateAdress(@RequestBody Address address){
+        return ResponseEntity.ok().body(addressRepository.save(address));
     }
 
     @DeleteMapping("/{id}")
-    public String deleteAdress(@PathVariable Long id){
+    public ResponseEntity<Void> deleteAdress(@PathVariable Long id){
         addressRepository.deleteById(id);
-        return "Deleted";
+        return ResponseEntity.ok().build();
     }
 
 }
