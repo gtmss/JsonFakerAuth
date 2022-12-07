5399c596ce854d12280b013d7a2e2a988275ae6d
Ion Gataman <ion.gatman1@isa.utm.md>
2022-12-07 19:19:39 +0200

    - Swagger added

--- src/main/java/com/example/fakerwithauthorization/controllers/CompanyController.java
+++ src/main/java/com/example/fakerwithauthorization/controllers/CompanyController.java
@@ -2,6 +2,7 @@ package com.example.fakerwithauthorization.controllers;
 
 import com.example.fakerwithauthorization.models.Company;
 import com.example.fakerwithauthorization.repository.CompanyRepository;
+import io.swagger.annotations.ApiImplicitParam;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +27,7 @@ public class CompanyController {
     }
 
     @GetMapping
+    @ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, paramType = "header", dataTypeClass = String.class, example = "Bearer access_token")
     public ResponseEntity<Page<Company>> getAllCompanies(Pageable pageable){
         return ResponseEntity.ok().body(companyRepository.findAll(pageable));
     }
