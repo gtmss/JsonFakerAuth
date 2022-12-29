277762226d4d82bb8b51bb9bea443d08787030da
Ion Gataman <ion.gatman1@isa.utm.md>
2022-12-29 16:15:52 +0200

    - Get User by ID endpoint added

--- src/main/java/com/example/fakerwithauthorization/repository/UsersRepository.java
+++ src/main/java/com/example/fakerwithauthorization/repository/UsersRepository.java
@@ -3,15 +3,15 @@ package com.example.fakerwithauthorization.repository;
 import com.example.fakerwithauthorization.models.Users;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
     Page findAll(Pageable pageable);
+
+    Optional<Users> findById(Long id);
 
 }
